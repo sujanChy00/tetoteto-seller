c67b2177ce64b89eb9e434efb00f5ac0916187cc
sujanChy00 <suzu9050@gmail.com>
2026-08-14 15:24:40 +0545

    Wrap home navigation buttons with Expo Router links

--- src/components/home/home-links.tsx
+++ src/components/home/home-links.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { SymbolView } from "expo-symbols";
 import { View } from "react-native";
 import { useCSSVariable } from "uniwind";
@@ -7,29 +8,32 @@ export const HomeLinks = () => {
   const primaryColor = useCSSVariable("--color-primary") as string;
   return (
     <View className="flex-row items-center gap-6 justify-between">
-      <Button.Secondary className="flex-1">
-        <SymbolView
-          tintColor={primaryColor}
-          size={20}
-          name={{
-            android: "shopping_cart",
-            ios: "cart.fill",
-          }}
-        />
-        <Button.SecondaryLabel>Orders</Button.SecondaryLabel>
-      </Button.Secondary>
-
-      <Button.Secondary className="flex-1">
-        <SymbolView
-          tintColor={primaryColor}
-          size={20}
-          name={{
-            ios: "shippingbox.fill",
-            android: "local_shipping",
-          }}
-        />
-        <Button.SecondaryLabel>Shipments</Button.SecondaryLabel>
-      </Button.Secondary>
+      <Link asChild href={"/order"}>
+        <Button.Secondary className="flex-1">
+          <SymbolView
+            tintColor={primaryColor}
+            size={20}
+            name={{
+              android: "shopping_cart",
+              ios: "cart.fill",
+            }}
+          />
+          <Button.SecondaryLabel>Orders</Button.SecondaryLabel>
+        </Button.Secondary>
+      </Link>
+      <Link asChild href={"/shipments"}>
+        <Button.Secondary className="flex-1">
+          <SymbolView
+            tintColor={primaryColor}
+            size={20}
+            name={{
+              ios: "shippingbox.fill",
+              android: "local_shipping",
+            }}
+          />
+          <Button.SecondaryLabel>Shipments</Button.SecondaryLabel>
+        </Button.Secondary>
+      </Link>
     </View>
   );
 };
